da522811eeca0cfee602e855826ad18b7a50d95e
harpy7777 <harpy7777@gmail.com>
2026-05-23 20:53:13 +0900

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <meta charSet="utf-8" />
         <meta name="description" content="강의하는 아이들 서울염창캠퍼스 - 내신부터 수능까지 과학의 모든 것을 함께합니다." />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       </Head>
       <body>
         <Main />
